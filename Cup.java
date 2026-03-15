242f58b99f9596165738dc45188b21161abda2b7
Cristian Camilo Ortiz Sánchez <scamilo027@gmail.com>
2026-03-14 20:51:30 -0500

    Implementación de los colores de las copas

--- Cup.java
+++ Cup.java
@@ -9,7 +9,7 @@ public class Cup {
 
     public Cup(int value,int tMaxHeight,int tWidth, int tHeight) {
         this.height = 2*value -1;
-        this.assignColor();
+        this.assignColor(value);
         this.cupRectangle = new Rectangle[3];
         this.createCup(tMaxHeight,tWidth, tHeight);
     }
@@ -17,17 +17,17 @@ public class Cup {
     private void createCup(int tMaxHeight,int tWidth, int tHeight) {
         int mid = (tWidth*20/2) - this.height*20/2;
         if (this.height > 1) {
-            Rectangle base = new Rectangle(20,this.height*20,"blue",30+mid,280-tHeight*20);
+            Rectangle base = new Rectangle(20,this.height*20,color,30+mid,280-tHeight*20);
             base.makeVisible();
             cupRectangle[0] = base;
-            Rectangle ladoA = new Rectangle(this.height*20,20,"blue",30+mid,300-this.height*20-tHeight*20);
+            Rectangle ladoA = new Rectangle(this.height*20,20,color,30+mid,300-this.height*20-tHeight*20);
             ladoA.makeVisible();
             cupRectangle[1] = ladoA;
-            Rectangle ladoB = new Rectangle(this.height*20,20,"blue",mid+10+this.height*20,300-this.height*20-tHeight*20);
+            Rectangle ladoB = new Rectangle(this.height*20,20,color,mid+10+this.height*20,300-this.height*20-tHeight*20);
             ladoB.makeVisible();
             cupRectangle[2] = ladoB;
         } else {
-            Rectangle base = new Rectangle(20,this.height*20,"blue",30+mid,280-tHeight*20);
+            Rectangle base = new Rectangle(20,this.height*20,color,30+mid,280-tHeight*20);
             base.makeVisible();
             cupRectangle[0] = base;
         }
@@ -39,8 +39,10 @@ public class Cup {
         }
     }
 
-    private void assignColor() {
-
+    private void assignColor(int value) {
+        String[] colors = {"black","red","blue","green","yellow","magenta"};
+        String color = colors[value - 1 % 6];
+        this.color = color;
     }
     
     public int getHeight() {
