ac76e7335f6df517ce1e592cf9af6355bb1fed90
Cristian Camilo Ortiz Sánchez <scamilo027@gmail.com>
2026-03-15 10:11:19 -0500

    Correccion de Lid

--- Lid.java
+++ Lid.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Lid {
     private static final int HEIGHT = 1;
     private int width;
@@ -6,11 +8,11 @@ public class Lid {
     private Cup cup;
     private boolean isVisible;
     
-    public Lid(int value, int tHeight, int tWidth) {
+    public Lid(int value, int towerHeight, int towerWidth) {
         this.width = 2*value - 1;
-        int mid= (tWidth*15/2)-this.width*15/2;
-        //this.assignColor();
-        this.rectangle = new Rectangle(20 * HEIGHT, this.width * 15, "red", 30+mid, 280);
+        this.assignColor(value);
+        int middle = (20*towerWidth - 20*this.width)/2;
+        this.rectangle = new Rectangle(20 * HEIGHT, this.width * 20, color, 30 + middle, 280 - towerHeight*20);
         rectangle.makeVisible();
     }
 
@@ -20,5 +22,11 @@ public class Lid {
 
     public void makeInvisible() {
         this.rectangle.makeInvisible();
+    }
+    
+    private void assignColor(int value) {
+        String[] colors = {"blue","green","red","yellow","magenta","black"};
+        String color = colors[value - 1 % 5];
+        this.color = color;
     }
 }
