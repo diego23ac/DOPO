b9c23d2ab65fb86db7b895b2731b7271d2e01b24
diego23ac <corso.diegob@gmail.com>
2026-03-15 02:13:53 -0500

    metodos ñidedcups y stackingitems

--- Cup.java
+++ Cup.java
@@ -1,4 +1,3 @@
-import java.util.*;
 
 public class Cup {
     private int height;
--- Tower.java
+++ Tower.java
@@ -88,7 +88,77 @@ public class Tower{
             counter += 1;
         }
     }
-
+    
+    public int[] lidedCups() {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+    
+        for (Cup cup : cups.values()) {
+            int cupH = cup.getHeight();
+            int value = (cupH + 1) / 2;
+    
+            if (existsLidWithWidth(cupH)) {
+                if (!result.contains(value)) {
+                    result.add(value);
+                }
+            }
+        }
+    
+        Collections.sort(result);
+    
+        int[] out = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            out[i] = result.get(i);
+        }
+        return out;
+    }
+    
+    public String[][] stackingItems() {
+        ArrayList<String[]> items = new ArrayList<String[]>();
+    
+        for (Cup cup : cups.values()) {
+            int value = (cup.getHeight() + 1) / 2;
+            items.add(new String[] {"cup", "" + value});
+        }
+    
+        for (Lid lid : lids.values()) {
+            int value = (lid.getWidth() + 1) / 2;
+            items.add(new String[] {"lid", "" + value});
+        }
+    
+        sortItemsByNumber(items);
+    
+        String[][] out = new String[items.size()][2];
+        for (int i = 0; i < items.size(); i++) {
+            out[i][0] = items.get(i)[0];
+            out[i][1] = items.get(i)[1];
+        }
+        return out;
+    }
+    
+    private void sortItemsByNumber(ArrayList<String[]> items) {
+        for (int i = 0; i < items.size(); i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < items.size(); j++) {
+                int numJ = Integer.parseInt(items.get(j)[1]);
+                int numMin = Integer.parseInt(items.get(minIndex)[1]);
+                if (numJ < numMin) {
+                    minIndex = j;
+                }
+            }
+            String[] temp = items.get(i);
+            items.set(i, items.get(minIndex));
+            items.set(minIndex, temp);
+        }
+    }
+    
+    private boolean existsLidWithWidth(int width) {
+        for (Lid lid : lids.values()) {
+            if (lid.getWidth() == width) {
+                return true;
+            }
+        }
+        return false;
+    }
     public int height(){
         return height;
     }
